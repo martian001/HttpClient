9567bc77a672ed615cc5a0693114e15e9b3c1e03
martian001 <Administrator@6F-JRYFPC>
2016-11-30 17:58:20 +0800

    优化

--- src/com/et/httpclient/dome/Login.java
+++ src/com/et/httpclient/dome/Login.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
@@ -19,6 +20,7 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHeader;
 import org.apache.http.message.BasicNameValuePair;
 import org.junit.Test;
 
@@ -34,36 +36,43 @@ import org.junit.Test;
 public class Login {
     @Test
     public void testName() throws Exception {
-        HttpClient httpclient = new DefaultHttpClient();
-        // 创建参数队列：设置登录参数
-        Map<String, String> param = new HashMap<>();
-        param.put("userName", "jigou1");
-        param.put("password", "123456");
-        String url = "http://localhost:8080/AOM/ignore/checkUserLogin.action";
-        HttpPost httppost = fillParam(param, url);
-
-        // 执行请求，得到响应
-        HttpResponse response = httpclient.execute(httppost);
-        // InputStream content = response.getEntity().getContent();
-        String set_cookie = response.getFirstHeader("Set-Cookie").getValue();
-        // 打印Cookie值
-        String cookieStr = set_cookie.substring(0, set_cookie.indexOf(";"));
-        System.out.println(cookieStr);
-        // 打印返回的结果：entity是发送或者接收消息的载体。entities?可以通过request和 response获取到
-        StringBuilder result = getResponseContent(response);
-        //        System.out.println(result);
-
-        // 获得Session之后，把该值保存下来。然后再访问其他受保护对象时设置头信息的Cookie字段就好了，代码片段如下
-        HttpClient httpclient2 = new DefaultHttpClient();
-        // 根据获得的Cookie值，设置头信息，然后发送请求，获得内容
-        url = "http://localhost:8080/AOM/financeController/orgMakeLoansList.action";
-        param = new HashMap<>();
-        param.put("page", "1");
-        param.put("rows", "10");
-        httppost = fillParam(param, url);
-        httppost.setHeader("Cookie", cookieStr);
-        HttpResponse response2 = httpclient2.execute(httppost);
-        System.out.println(getResponseContent(response2));
+        try {
+            HttpClient httpclient = new DefaultHttpClient();
+            // 创建参数队列：设置登录参数
+            Map<String, String> param = new HashMap<>();
+            param.put("user_id", "20196");
+            //        param.put("password", "MTIzNDU2");
+            String url = "http://localhost:8088/BMS/mobileApi/getUserInfo1.action";
+            HttpPost httppost = fillParam(param, url);
+//            Header header1 = new BasicHeader("Content-Type", "application/json");
+//            httppost.addHeader(header1);
+//            Header header2 = new BasicHeader("charset", "utf-8");
+//            httppost.addHeader(header2);
+//            Header header3 = new BasicHeader("Accept-Version", "api_v1");
+//            httppost.addHeader(header3);
+//            // 执行请求，得到响应
+//            HttpResponse response = httpclient.execute(httppost);
+//            // 打印返回的结果：entity是发送或者接收消息的载体。entities?可以通过request和 response获取到
+//            StringBuilder result = getResponseContent(response);
+//            System.out.println(result);
+//            // InputStream content = response.getEntity().getContent();
+//            String set_cookie = response.getFirstHeader("Set-Cookie").getValue();
+//            // 打印Cookie值
+//            String cookieStr = set_cookie.substring(0, set_cookie.indexOf(";"));
+//            System.out.println(cookieStr);
+            // 获得Session之后，把该值保存下来。然后再访问其他受保护对象时设置头信息的Cookie字段就好了，代码片段如下
+            HttpClient httpclient2 = new DefaultHttpClient();
+            // 根据获得的Cookie值，设置头信息，然后发送请求，获得内容
+            url = "http://localhost:8088/BMS/mobileApi/getSignDays.action";
+            param = new HashMap<>();
+            param.put("userId", "20196");
+            httppost = fillParam(param, url);
+            httppost.setHeader("Cookie", "JSESSIONID=8A4882EC0377F088F472290691D9D32E");
+            HttpResponse response2 = httpclient2.execute(httppost);
+            System.out.println(getResponseContent(response2));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
@@ -71,27 +80,21 @@ public class Login {
         HttpClient httpclient = new DefaultHttpClient();
         // 创建参数队列：设置登录参数
         Map<String, String> param = new HashMap<>();
-        String url = "http://0755.qfang.com/qfang-infra/login/loginTimeOutValidation?userName=13145860701&password=995396426";
-        HttpGet httpGet = new HttpGet(url);
-        // 执行请求，得到响应
-        HttpResponse response = httpclient.execute(httpGet);
-        String set_cookie = response.getFirstHeader("Set-Cookie").getValue();
-        // 打印Cookie值
-        String cookieStr = set_cookie.substring(0, set_cookie.indexOf(";"));
-        System.out.println(cookieStr);
+        String url = "";
 
         // 获得Session之后，把该值保存下来。然后再访问其他受保护对象时设置头信息的Cookie字段就好了，代码片段如下
         httpclient = new DefaultHttpClient();
         // 根据获得的Cookie值，设置头信息，然后发送请求，获得内容
-        url = "http://0755.qfang.com/qfang-hr/f7/hr/personData";
+        url = "http://0755.testerp.qfang.com/qfang-hr/f7/hr/personData";//测试环境
+        //url = "http://0755.qfang.com/qfang-hr/f7/hr/personData";//正式环境
         HttpPost httppost = fillParam(param, url);
         param = new HashMap<>();
         param.put("checkValue", "checked");
-        param.put("parentId", "87dbe022-03e2-4b6e-9092-65455042ad52");
-        param.put("pagerNumber", "2");
-        param.put("perPage", "13");
+        param.put("parentId", "834e675b-2cdd-460e-adaf-d1b6cca2d072");
+        param.put("pagerNumber", "1");
+        param.put("perPage", "1000");
         httppost = fillParam(param, url);
-        httppost.setHeader("Cookie", cookieStr);
+        httppost.setHeader("Cookie", "JSESSIONID=aaaSmAJOfxh5K5T3pM0Iv; COMMONSESSION=aaaSmAJOfxh5K5T3pM0Iv; personId=4b2ee417-aff5-472b-9f93-eb0a823d7cb0; loginHistory_19999999999=19999999999%7C%E6%B7%B1%E5%9C%B3%E4%B8%93%E5%91%98; noClew=flase; 4b2ee417-aff5-472b-9f93-eb0a823d7cb0=");
         HttpResponse response2 = httpclient.execute(httppost);
         System.out.println(getResponseContent(response2));
     }
